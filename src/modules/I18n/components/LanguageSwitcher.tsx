f064fbfcdc8941ecb19e5913bdad430ace88a5cb
Martin Ratinaud <martinratinaud@gmail.com>
2023-03-17 11:54:15 +0400

    Add next-translate

---
+++ src/modules/I18n/components/LanguageSwitcher.tsx
@@ -0,0 +1,22 @@
+import useTranslation from 'next-translate/useTranslation';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const ChangeLanguage = () => {
+  const { t } = useTranslation();
+  const { locale, locales } = useRouter();
+
+  if (!locales) return null;
+
+  return locales.map((lng) => {
+    if (lng === locale) return null;
+
+    return (
+      <Link href="/" locale={lng} key={lng}>
+        {t(`common:cta`, { lang: lng })}
+      </Link>
+    );
+  });
+};
+
+export default ChangeLanguage;
---
+++ src/modules/I18n/index.tsx
@@ -0,0 +1,3 @@
+export { default as useTranslation } from 'next-translate/useTranslation';
+
+export { default as LanguageSwitcher } from './components/LanguageSwitcher';
